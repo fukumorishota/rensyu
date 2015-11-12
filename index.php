3bb67f38eeb5c0e5028b55291e9916fd47be8417
福森翔太 <fukumorishota@gmail.com>
2015-11-12 23:32:40 +0900

    kagosima

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE HTML>
-<<<<<<< HEAD
-<i>aiueo
-=======
-<i>僕の名前は福森翔太
->>>>>>> 714f11704852c129e7d650efdfc5067508493f00
+<html lang="ja">
+<head>
+
+</head>
+<script>
+	document.write("Javascript");
+</script>
+<body>
+
+</body>
+</html>
